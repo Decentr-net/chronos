c19a778ea6fb7fb08f1f1f5bbdfb68a505203a36
Timaley <timaley@mail.ru>
2021-03-04 17:54:42 +0300

    DEC-224: Create dashboard component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainLayoutComponent } from './_core/layout/main-layout';
+import { AppRoute } from './app-route';
 
 const routes: Routes = [
   {
     path: '',
     component: MainLayoutComponent,
-    children: []
+    children: [
+      {
+        path: '',
+        redirectTo: AppRoute.Dashboard,
+        pathMatch: 'full',
+      },
+      {
+        path: AppRoute.Dashboard,
+        loadChildren: () => import('./dashboard').then(m => m.DashboardModule),
+      }
+    ]
   }
 ];
 
