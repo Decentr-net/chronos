995e92c78fbd87174f198e9f1c50a90e327e6542
Timaley <timaley@mail.ru>
2021-03-18 15:32:46 +0300

    DEC-224: Move card component to shared

--- src/app/dashboard/components/index.ts
+++ src/app/dashboard/components/index.ts
@@ -1,7 +1,9 @@
 import { CurrencyChartComponent } from './currency-chart';
+import { InfoCardComponent } from '@shared/components/info-card';
 
 export * from './currency-chart';
 
 export const DASHBOARD_COMPONENTS = [
   CurrencyChartComponent,
+  InfoCardComponent,
 ];
--- src/app/dashboard/pages/index.ts
+++ src/app/dashboard/pages/index.ts
@@ -1,9 +1,7 @@
 import { DashboardPageComponent } from './dashboard-page';
-import { InfoCardComponent } from '@shared/components/info-card';
 
 export * from './dashboard-page/';
 
 export const DASHBOARD_PAGES = [
   DashboardPageComponent,
-  InfoCardComponent,
 ];
