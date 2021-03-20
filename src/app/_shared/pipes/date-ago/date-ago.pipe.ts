f99ef44aa3823a925196dd5d252f02c150bee605
Alexander Khomovnenko <a.khomovnenko@aiolane.com>
2021-03-20 12:21:41 +0300

    feature: DEC-224: dateAgo pipe

--- src/app/_shared/pipes/date-ago/date-ago.pipe.ts
+++ src/app/_shared/pipes/date-ago/date-ago.pipe.ts
@@ -1,31 +1,92 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { ChangeDetectorRef, NgZone, OnDestroy, Pipe, PipeTransform } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Subscription, timer } from 'rxjs';
 
 @Pipe({
   name: 'dateAgo',
-  pure: true
+  pure: false,
 })
-export class DateAgoPipe implements PipeTransform {
+export class DateAgoPipe implements PipeTransform, OnDestroy {
+  private ngZone$: Subscription = Subscription.EMPTY;
 
-  transform(value: string, args?: any): any {
-    if (value) {
-      const seconds = Math.floor((+new Date() - +new Date(value)) / 1000);
-      const intervals = {
-        year: 31536000,
-        month: 2592000,
-        week: 604800,
-        day: 86400,
-        hour: 3600,
-        minute: 60,
-        second: 1
-      };
-      for (const i of Object.keys(intervals)) {
-        const counter = Math.floor(seconds / intervals[i]);
-        if (counter > 0) {
-          return counter === 1 ? counter + ' ' + i + ' ago' : counter + ' ' + i + 's ago';
-        }
-      }
-    }
-    return value;
+  constructor(
+    private changeDetectorRef: ChangeDetectorRef,
+    private ngZone: NgZone,
+  ) {
   }
 
+  transform(value: string | number): string {
+    const date = new Date(value);
+    const now = new Date();
+    const seconds = Math.round(Math.abs((now.getTime() - date.getTime()) / 1000));
+    const timeToUpdate = (Number.isNaN(seconds)) ? 1000 : this.getSecondsUntilUpdate(seconds) * 1000;
+
+    const minutes = Math.floor(Math.abs(seconds / 60));
+    const hours = Math.floor(Math.abs(minutes / 60));
+    const days = Math.round(Math.abs(hours / 24));
+    const months = Math.round(Math.abs(days / 30.416));
+    const years = Math.round(Math.abs(days / 365));
+
+    this.removeTimer();
+    this.ngZone$ = this.ngZone.runOutsideAngular(() => this.ngZone$ = timer(timeToUpdate)
+      .pipe(
+        map(() => this.ngZone.run(() => this.changeDetectorRef.markForCheck())),
+      )
+      .subscribe()
+    );
+
+    let output = '';
+
+    if (Number.isNaN(seconds)) {
+      return '';
+    } else if (seconds <= 45) {
+      output = `${seconds}s ago`;
+    } else if (seconds <= 90) {
+      output = 'a minute ago';
+    } else if (minutes < 60) {
+      output = `${minutes}m ago`;
+    } else if (minutes < 120) {
+      output = 'an hour ago';
+    } else if (hours < 24) {
+      output = `${hours}h ago`;
+    } else if (hours < 48) {
+      output = 'a day ago';
+    } else if (days <= 28) {
+      output = `${days} days ago`;
+    } else if (days <= 45) {
+      output = 'a month ago';
+    } else if (days <= 345) {
+      output = `${months} months ago`;
+    } else if (days <= 545) {
+      output = 'a year ago';
+    } else {
+      output = `${years} years ago`;
+    }
+
+    return output;
+  }
+
+  ngOnDestroy(): void {
+    this.removeTimer();
+  }
+
+  private getSecondsUntilUpdate(seconds: number): number {
+    const minute = 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    if (seconds < minute) {
+      return 5; // less than 1 minute, update every 5 seconds
+    } else if (seconds < hour) {
+      return 30; // less than an hour, update every 30 seconds
+    } else if (seconds < day) {
+      return 300; // less then a day, update every 5 minutes
+    } else {
+      return 3600; // update every hour
+    }
+  }
+
+  private removeTimer(): void {
+    this.ngZone$.unsubscribe();
+  }
 }
