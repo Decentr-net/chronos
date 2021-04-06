b21834cac06218d931d6d3e7c5a9843da52f0287
Alexander Khomovnenko <a.khomovnenko@aiolane.com>
2021-04-06 15:44:36 +0300

    feature: CHRNS-16: getConfig from S3

--- src/app/_core/services/configuration/configuration-api.service.ts
+++ src/app/_core/services/configuration/configuration-api.service.ts
@@ -1,50 +1,30 @@
 import { Injectable } from '@angular/core';
-// import { HttpClient } from '@angular/common/http';
-import {
-  // defer,
-  Observable,
-  of,
-} from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
-// import { Environment } from '@environments/environments.definitions';
+import { Environment } from '@environments/environments.definitions';
 import { Configuration } from './configuration.definitions';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ConfigurationApiService {
-  // constructor(
-    // private environment: Environment,
-    // private httpClient: HttpClient,
-  // ) {
-  // }
+  constructor(
+    private environment: Environment,
+    private httpClient: HttpClient,
+  ) {
+  }
 
   public getConfig(): Observable<Configuration> {
-    // const now = Date.now();
-    // const headers = {
-    //   'Cache-Control': 'no-cache',
-    //   Pragma: 'no-cache',
-    //   Expires: '0'
-    // };
-    //
-    // return this.httpClient.get<Configuration>(`${this.environment.awsStorage}/config.json?${now}`, {
-    //   headers,
-    // });
+    const now = Date.now();
+    const headers = {
+      'Cache-Control': 'no-cache',
+      Pragma: 'no-cache',
+      Expires: '0',
+    };
 
-    return of({
-      network: {
-        rest: [
-          'https://ares.testnet.decentr.xyz',
-          'https://poseidon.testnet.decentr.xyz',
-          'https://hera.testnet.decentr.xyz',
-          'https://zeus.testnet.decentr.xyz',
-          'https://hermes.testnet.decentr.xyz',
-        ],
-      },
-      maintenance: false,
-      theseus: {
-        url: 'https://theseus.testnet.decentr.xyz',
-      },
+    return this.httpClient.get<Configuration>(`${this.environment.awsStorage}/config.json?${now}`, {
+      headers,
     });
   }
 }
