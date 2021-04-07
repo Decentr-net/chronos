ce95d0fcb193d88afa41b55f085353b91e3daa60
Eugene <eugene.tishkevich@gmail.com>
2021-04-07 15:38:18 +0300

    feature: CHRNS-19: mobile menu (#22)

---
+++ src/app/_core/layout/layout-header/components/navigation.ts
@@ -0,0 +1,28 @@
+import { AppRoute } from '../../../../app-route';
+import { TrackByFunction } from '@angular/core';
+
+export interface NavigationMenuElement {
+  name: string;
+  url: AppRoute;
+}
+
+export const NAVIGATION_ELEMENTS: NavigationMenuElement[] = [
+  {
+    name: 'Dashboard',
+    url: AppRoute.Dashboard,
+  },
+  {
+    name: 'Blocks',
+    url: AppRoute.Blocks,
+  },
+  {
+    name: 'Transactions',
+    url: AppRoute.Transactions,
+  },
+  {
+    name: 'Validators',
+    url: AppRoute.Validators,
+  },
+];
+
+export const trackByNavigationElement: TrackByFunction<NavigationMenuElement> = ({}, { name }) => name;
--- src/app/_core/layout/layout-header/layout-header.component.ts
+++ src/app/_core/layout/layout-header/layout-header.component.ts
@@ -1,24 +1,30 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 import { SvgIconRegistry } from '@ngneat/svg-icon';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 
 import { svgCloseIcon } from '@shared/svg-icons/close';
 import { svgLogoIcon } from '@shared/svg-icons/logo';
 import { svgMenuIcon } from '@shared/svg-icons/menu';
 import { svgSearchIcon } from '@shared/svg-icons/search';
 
+@UntilDestroy()
 @Component({
   selector: 'app-layout-header',
   templateUrl: './layout-header.component.html',
   styleUrls: ['./layout-header.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LayoutHeaderComponent {
+export class LayoutHeaderComponent implements OnInit {
   public isMobileMenuOpen = false;
   public isMobileSearchOpened = false;
 
   constructor(
+    public elementRef: ElementRef,
     private changeDetectorRef: ChangeDetectorRef,
     private svgIconRegistry: SvgIconRegistry,
+    private router: Router,
   ) {
     svgIconRegistry.register([
       svgCloseIcon,
@@ -28,8 +34,11 @@ export class LayoutHeaderComponent {
     ]);
   }
 
-  public onMenuActiveZoneChange(active: boolean): void {
-    this.isMobileMenuOpen = active;
+  public ngOnInit(): void {
+    this.router.events.pipe(
+      filter((event) => event instanceof NavigationEnd),
+      untilDestroyed(this),
+    ).subscribe(() => this.closeMenu());
   }
 
   public openMenu(): void {
@@ -38,14 +47,11 @@ export class LayoutHeaderComponent {
 
   public closeMenu(): void {
     this.isMobileMenuOpen = false;
+    this.changeDetectorRef.markForCheck();
   }
 
   public toggleMenu(): void {
     this.isMobileMenuOpen = !this.isMobileMenuOpen;
-  }
-
-  public onObscured(obscured: boolean): void {
-    this.isMobileMenuOpen = !obscured;
   }
 
   public openMobileSearch(): void {
