8608aa46ee344052abf10a7b43de3d53c503e32c
catrias <eugene.tishkevich@gmail.com>
2021-04-14 19:11:18 +0300

    feature: blocks page: responsive design

--- src/app/blocks/pages/blocks-page/blocks-page.component.ts
+++ src/app/blocks/pages/blocks-page/blocks-page.component.ts
@@ -4,6 +4,7 @@ import { Block } from 'decentr-js';
 
 import { ONE_SECOND } from '@shared/utils/date';
 import { BlocksService } from '@core/services/blocks';
+import { Breakpoint, BreakpointService } from '@shared/directives/breakpoint';
 
 @Component({
   selector: 'app-blocks-page',
@@ -14,12 +15,17 @@ import { BlocksService } from '@core/services/blocks';
 export class BlocksPageComponent implements OnInit {
   blocks$: Observable<Block[]>;
 
+  public isMobile$: Observable<boolean>;
+
   constructor(
     private blocksService: BlocksService,
+    private breakpointService: BreakpointService,
   ) {
   }
 
   public ngOnInit(): void {
     this.blocks$ = this.blocksService.getLatestBlocksLive(50, ONE_SECOND * 10);
+
+    this.isMobile$ = this.breakpointService.observe(Breakpoint.Mobile);
   }
 }
