af511b7f78b2d66a54df48c7b4202cbca1028d05
Alexander Khomovnenko <a.khomovnenko@aiolane.com>
2021-04-15 20:27:48 +0300

    feature: CHRNS-46: update icons

--- src/app/_shared/svg-icons/unbonding.ts
+++ src/app/_shared/svg-icons/unbonding.ts
@@ -1,4 +1,4 @@
 export const svgUnbondingIcon = {
-    data: `<svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 0C4.489 0 0 4.489 0 10s4.489 10 10 10 10-4.489 10-10S15.511 0 10 0zm0 2c4.43 0 8 3.57 8 8s-3.57 8-8 8-8-3.57-8-8 3.57-8 8-8zM9 4v6.414l4.293 4.293 1.414-1.414L11 9.586V4H9z" fill="currentColor"/></svg>`,
+    data: `<svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 2C6.489 2 2 6.489 2 12s4.489 10 10 10 10-4.489 10-10S17.511 2 12 2zm0 2c4.43 0 8 3.57 8 8s-3.57 8-8 8-8-3.57-8-8 3.57-8 8-8zm-1 2v6.414l4.293 4.293 1.414-1.414L13 11.586V6h-2z" fill="currentColor"/></svg>`,
     name: 'unbonding'
 };
