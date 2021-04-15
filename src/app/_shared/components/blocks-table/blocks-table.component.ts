76f152bbbbe43c1674e07bc51b74f586fb4ba524
Alexander Khomovnenko <a.khomovnenko@aiolane.com>
2021-04-15 20:15:47 +0300

    feature: CHRNS-46: responsive display tx/block hash

--- src/app/_shared/components/blocks-table/blocks-table.component.ts
+++ src/app/_shared/components/blocks-table/blocks-table.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component, Input, TrackByFunction } from '@ang
 import { Block } from 'decentr-js';
 
 import { AppRoute } from '../../../app-route';
+import { Breakpoint } from '@shared/directives/breakpoint';
 
 @Component({
   selector: 'app-blocks-table',
@@ -11,9 +12,14 @@ import { AppRoute } from '../../../app-route';
 })
 export class BlocksTableComponent {
   @Input() data: Block[];
+  @Input() hashSize: Record<Breakpoint.Desktop | Breakpoint.Mobile | Breakpoint.Tablet, number> = {
+    [Breakpoint.Desktop]: 70,
+    [Breakpoint.Tablet]: 50,
+    [Breakpoint.Mobile]: 15,
+  };
 
   public blocksRoute: AppRoute = AppRoute.Blocks;
-
+  public breakpoint: typeof Breakpoint = Breakpoint;
   public columns: string[] = ['Height', 'Hash', 'Time'];
 
   public trackByHash: TrackByFunction<Block> = ({}, { block_id }) => block_id.hash;
--- src/app/_shared/components/transactions-table/transactions-table.component.ts
+++ src/app/_shared/components/transactions-table/transactions-table.component.ts
@@ -12,6 +12,11 @@ import { Breakpoint } from '@shared/directives/breakpoint';
 })
 export class TransactionsTableComponent {
   @Input() data: Transaction[] = [];
+  @Input() hashSize: Record<Breakpoint.Desktop | Breakpoint.Mobile | Breakpoint.Tablet, number> = {
+    [Breakpoint.Desktop]: 25,
+    [Breakpoint.Tablet]: 15,
+    [Breakpoint.Mobile]: 9,
+  };
 
   public appRoute: typeof AppRoute = AppRoute;
   public breakpoint: typeof Breakpoint = Breakpoint;
