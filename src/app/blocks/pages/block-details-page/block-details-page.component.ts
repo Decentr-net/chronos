5bf3a1c3e29e6f47fa5571363bfbefac1b90105d
Eugene <eugene.tishkevich@gmail.com>
2021-04-19 13:46:24 +0300

    feature: block details page: pagination (#37)

--- src/app/blocks/pages/block-details-page/block-details-page.component.ts
+++ src/app/blocks/pages/block-details-page/block-details-page.component.ts
@@ -1,16 +1,20 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Block, Transaction } from 'decentr-js';
 import { EMPTY, Observable } from 'rxjs';
 import { catchError, map, pluck, switchMap } from 'rxjs/operators';
 import { SvgIconRegistry } from '@ngneat/svg-icon';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 
 import { Breakpoint, BreakpointService } from '@shared/directives/breakpoint';
+import { svgArrowLeftIcon } from '@shared/svg-icons/arrow-left';
+import { svgExpandLeftIcon } from '@shared/svg-icons/expand-left';
+import { svgExpandRightIcon } from '@shared/svg-icons/expand-right';
 import { BlocksService } from '@core/services/blocks';
 import { TransactionsService } from '@core/services/transactions';
-import { svgArrowLeftIcon } from '@shared/svg-icons/arrow-left';
 import { AppRoute } from '../../../app-route';
 
+@UntilDestroy()
 @Component({
   selector: 'app-block-details-page',
   templateUrl: './block-details-page.component.html',
@@ -21,6 +25,9 @@ export class BlockDetailsPageComponent implements OnInit {
   public blockDetails$: Observable<Block>;
   public blockTxs$: Observable<Transaction[]>;
 
+  public nextBlockHeight: number;
+  public previousBlockHeight: number;
+
   public isTablet$: Observable<boolean>;
   public breakpoint: typeof Breakpoint = Breakpoint;
 
@@ -28,20 +35,26 @@ export class BlockDetailsPageComponent implements OnInit {
     private activatedRoute: ActivatedRoute,
     private blocksService: BlocksService,
     private breakpointService: BreakpointService,
+    private changeDetectorRef: ChangeDetectorRef,
     private router: Router,
     private svgIconRegistry: SvgIconRegistry,
     private transactionsService: TransactionsService,
   ) {
-    svgIconRegistry.register(svgArrowLeftIcon);
+    svgIconRegistry.register([
+      svgArrowLeftIcon,
+      svgExpandLeftIcon,
+      svgExpandRightIcon,
+    ]);
   }
 
   public ngOnInit(): void {
     const height$ = this.activatedRoute.params.pipe(
       pluck('blockHeight'),
+      map((blockHeight) => +blockHeight),
     );
 
     this.blockDetails$ = height$.pipe(
-      switchMap((height) => this.blocksService.getBlockByHeight(height)),
+      switchMap((height) => this.blocksService.getBlockByHeight(height.toString())),
       catchError(() => {
         this.router.navigate(['/', AppRoute.Empty], {
           skipLocationChange: true,
@@ -59,6 +72,19 @@ export class BlockDetailsPageComponent implements OnInit {
       map((transactions) => transactions.txs),
       catchError(() => EMPTY),
     );
+
+    height$.pipe(
+      switchMap((height) => this.blocksService.getLatestBlock().pipe(
+        pluck('block', 'header', 'height'),
+        map((latestBlockHeight) => [+latestBlockHeight, height]),
+      )),
+      untilDestroyed(this),
+    ).subscribe(([latestBlockHeight, height]) => {
+      this.nextBlockHeight = height === latestBlockHeight ? undefined : height + 1;
+      this.previousBlockHeight = height - 1 || undefined;
+
+      this.changeDetectorRef.markForCheck();
+    });
 
     this.isTablet$ = this.breakpointService.observe(Breakpoint.Tablet);
   }
