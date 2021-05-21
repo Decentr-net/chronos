daad32c2d2f79cfab56b2701e7ad4998dad3b22a
Alexander Khomovnenko <a.khomovnenko@aiolane.com>
2021-05-21 12:40:48 +0300

    bugfix: CHRNS-70: disable custom scroll for tablets/mobiles

--- src/app/_core/layout/main-layout/main-layout.module.ts
+++ src/app/_core/layout/main-layout/main-layout.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { NgScrollbarModule } from 'ngx-scrollbar';
 import { RouterModule } from '@angular/router';
 
+import { BreakpointModule } from '@shared/directives/breakpoint';
 import { LayoutHeaderModule } from '../layout-header';
 import { LayoutFooterModule } from '../layout-footer';
 import { MainLayoutComponent } from './main-layout.component';
 
 @NgModule({
   imports: [
+    BreakpointModule,
     LayoutFooterModule,
     LayoutHeaderModule,
     NgScrollbarModule,
