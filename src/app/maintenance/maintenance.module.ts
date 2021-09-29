535da533691215ab1f14802f70a141d7c14fec7f
Eugene Tishkevich <eugene.tishkevich@gmail.com>
2021-09-29 14:10:33 +0300

    feature: maintenance: add network selector

--- src/app/maintenance/maintenance.module.ts
+++ src/app/maintenance/maintenance.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { SvgIconsModule } from '@ngneat/svg-icon';
 
+import { NetworkSelectorModule } from '@shared/components/network-selector';
 import { MAINTENANCE_PAGES } from './pages';
 import { MaintenanceRoutingModule } from './maintenance-routing.module';
 
 @NgModule({
   imports: [
     MaintenanceRoutingModule,
+    NetworkSelectorModule,
     SvgIconsModule,
   ],
   declarations: [
