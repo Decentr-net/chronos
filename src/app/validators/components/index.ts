27363930e93d914251e7fe727c1647ff9da77dfd
Alexander Khomovnenko <a.khomovnenko@aiolane.com>
2021-10-11 23:26:19 +0300

    feature: CHRNS-117: validators, validator details pages: display jailed label

--- src/app/validators/components/index.ts
+++ src/app/validators/components/index.ts
@@ -1,7 +1,9 @@
+import { ValidatorJailedComponent } from './validator-jailed';
 import { ValidatorStatusComponent } from './validator-status';
 import { ValidatorsTableComponent } from './validators-table';
 
 export const VALIDATORS_COMPONENTS = [
+  ValidatorJailedComponent,
   ValidatorStatusComponent,
   ValidatorsTableComponent,
 ];
