f925d0dae5f441f438ea930a791d9607d2897e32
Eugene Tishkevich <eugene.tishkevich@gmail.com>
2021-10-20 19:01:45 +0300

    feature: CHRNS-22: display currency label for transaction fee

--- src/app/transactions/transactions.module.ts
+++ src/app/transactions/transactions.module.ts
@@ -4,6 +4,7 @@ import { SvgIconsModule } from '@ngneat/svg-icon';
 import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
 
 import { ButtonBackModule } from '@shared/components/button-back';
+import { CurrencySymbolModule } from '@shared/pipes/currency-symbol';
 import { DetailsTableModule } from '@shared/components/details-table';
 import { InfoCardModule } from '@shared/components/info-card';
 import { NumberFormatModule } from '@shared/pipes/number-format';
@@ -11,16 +12,17 @@ import { PageLinkModule } from '@shared/components/page-link';
 import { TransactionMessageDetailsModule } from '@shared/components/transaction-message-details';
 import { TransactionMessageTypeIconModule } from '@shared/components/transaction-message-type-icon';
 import { TransactionsTableModule } from '@shared/components/transactions-table';
+import { TxMessageTypeLabelModule } from '@shared/pipes/tx-message-type-label';
 import { TypefaceModule } from '@shared/directives/typeface';
 import { TransactionsRoutingModule } from './transactions-routing.module';
 import { TRANSACTION_COMPONENTS } from './components';
 import { TRANSACTIONS_PAGES } from './pages';
-import { TxMessageTypeLabelModule } from '@shared/pipes/tx-message-type-label';
 
 @NgModule({
   imports: [
     ButtonBackModule,
     CommonModule,
+    CurrencySymbolModule,
     DetailsTableModule,
     InfoCardModule,
     NgxSkeletonLoaderModule,
